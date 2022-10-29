d9a1a2e1651e260c35d02c267693b308084a7c92
alphak3y <alphak3y@protonmail.com>
2022-10-29 11:33:26 -0600

    add custom types

--- src/pool.rs
+++ src/pool.rs
@@ -2,10 +2,10 @@ extern crate alloc;
 use fuel_indexer_macros::{graphql_schema, handler};
 use fuels_abigen_macro::wasm_abigen;
 
-graphql_schema!("concentrated_liquidity", "schema/schema.graphql");
+graphql_schema!("concentrated_liquidity", "../schema/schema.graphql");
 wasm_abigen!(
     no_name,
-    "hello-indexer/contracts/hello_indexer.json"
+    "../contracts/concentrated_liquidity.json"
 );
 
 #[handler]
@@ -19,7 +19,7 @@ fn handle_init(event: InitEvent) {
         tick_spacing,
         init_price_lower,
         init_price_upper,
-        init_tick
+        init_tick_underlying
     } = event;
 
     let mut pool = match Pool::load(contract_id) {
@@ -29,11 +29,25 @@ fn handle_init(event: InitEvent) {
             token0: ContractId::from(token0),
             token1: ContractId::from(token1),
             swap_fee: swap_fee,
-            liquidity: U128::from(0,0),
-            sqrt_price: Q64x64::from(init_price_upper, init_price_lower),
-            fee_growth_global0: Q64x64::from(0,0),
-            fee_growth_global1: Q64x64::from(0,0),
-            nearest_tick: I24::from(init_tick)
+            liquidity: U128 {
+                upper: 0,
+                lower: 0
+            },
+            sqrt_price: Q64x64 { 
+                integer: init_price_upper,
+                decimal: init_price_lower
+            },
+            fee_growth_global0: Q64x64 {
+                integer: 0,
+                decimal: 0
+            },
+            fee_growth_global1: Q64x64 {
+                integer: 0,
+                decimal: 0
+            },
+            nearest_tick: I24 {
+                underlying: init_tick_underlying
+            }
         },
     };
 
